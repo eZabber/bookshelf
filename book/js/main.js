4422e93e5a4a893986376c5eb32771425e05cd37
eZabber <128634426+eZabber@users.noreply.github.com>
2026-01-11 19:40:31 +0200

    Update main.js

--- book/js/main.js
+++ book/js/main.js
@@ -1,1 +1,21 @@
 //# Entry point: imports everything, wires events
+import { $ } from './dom-utils.js';
+import { t } from './i18n.js';
+import { library, loadLibrary } from './state.js';
+import { setLanguage } from './i18n.js';
+import { renderBooks } from './render.js';
+import { injectCloudControls } from './init.js'; // if separate
+import { gapiLoaded, gisLoaded } from './drive.js';
+
+// Expose for Google
+window.gapiLoaded = gapiLoaded;
+window.gisLoaded = gisLoaded;
+
+// Init
+window.addEventListener("DOMContentLoaded", () => {
+  library = loadLibrary();
+  injectCloudControls();
+  setLanguage(currentLang);
+  renderBooks();
+  // All other wiring...
+});
