74b044b2ab4c00eaf4896263e761a721907fc5e3
eZabber <128634426+eZabber@users.noreply.github.com>
2026-01-11 19:35:19 +0200

    Update i18n.js

--- book/js/i18n.js
+++ book/js/i18n.js
@@ -1,1 +1,15 @@
 //# Translations + t() + currentLang
+import { LS } from './config.js';
+
+export const TRANSLATIONS = { /* your full TRANSLATIONS object here – copy from your code */ };
+
+export let currentLang = localStorage.getItem(LS.LANG) || "en";
+
+export function setCurrentLang(lang) {
+  currentLang = lang;
+  localStorage.setItem(LS.LANG, lang);
+}
+
+export function t(key) {
+  return TRANSLATIONS[currentLang]?.[key] ?? key;
+}
