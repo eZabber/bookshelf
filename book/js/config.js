6b045213d20ca308164b00f31bd314621587c794
eZabber <128634426+eZabber@users.noreply.github.com>
2026-01-11 19:35:02 +0200

    Update config.js

--- book/js/config.js
+++ book/js/config.js
@@ -1,1 +1,13 @@
 //# Constants (CLIENT_ID, scopes, LS keys)
+export const CLIENT_ID = "579369345257-sqq02cnitlhcf54o5ptad36fm19jcha7.apps.googleusercontent.com";
+export const DEVELOPER_KEY = "";
+export const DRIVE_SCOPE = "https://www.googleapis.com/auth/drive.appdata";
+export const DISCOVERY = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+export const CLOUD_JSON_NAME = "my_bookshelf.json";
+
+export const LS = {
+  LANG: "appLang",
+  LIB: "myLibrary",
+  DARK: "darkMode",
+  CLOUD_FILE_ID: "cloudFileId_appdata"
+};
